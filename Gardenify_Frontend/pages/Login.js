defad593f9d266871c788bfd4da55b11734c6c7b
Dishant Yadav <dishantyadav599@gmail.com>
2022-11-30 18:52:59 +0530

    Able to get the geolocation

--- Gardenify_Frontend/pages/Login.js
+++ Gardenify_Frontend/pages/Login.js
@@ -8,8 +8,10 @@ import {
   Image,
   TextInput,
   TouchableOpacity,
+  PermissionsAndroid
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Geolocation from 'react-native-geolocation-service';
 
 import Buttons from '../components/Buttons.js';
 import {
@@ -40,9 +42,59 @@ const loginUser = async (email, password) => {
   return data.success === true;
 };
 
+// Function to get permission for location
+const requestLocationPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Geolocation Permission',
+        message: 'Can we access your location?',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    console.log('granted', granted);
+    if (granted === 'granted') {
+      console.log('You can use Geolocation');
+      return true;
+    } else {
+      console.log('You cannot use Geolocation');
+      return false;
+    }
+  } catch (err) {
+    return false;
+  }
+};
+
 const Login = ({navigation}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [location, setLocation] = useState(false);
+
+  // function to check permissions and get Location
+  const getLocation = () => {
+    const result = requestLocationPermission();
+    result.then(res => {
+      console.log('res is:', res);
+      if (res) {
+        Geolocation.getCurrentPosition(
+          position => {
+            console.log(position);
+            setLocation(position);
+          },
+          error => {
+            // See error code charts below.
+            console.log(error.code, error.message);
+            setLocation(false);
+          },
+          {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+        );
+      }
+    });
+    console.log(location);
+  };
 
   return (
     <ScrollView
@@ -125,8 +177,8 @@ const Login = ({navigation}) => {
             btn_text={'Login'}
             on_press={async () => {
               console.log('Clicked Login');
+              getLocation();
               const success = await loginUser(email, password);
-              // console.log(success);
               if (success) {
                 navigation.navigate('Home');
               }
