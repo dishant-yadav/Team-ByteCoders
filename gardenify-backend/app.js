c2da10e70537f8d1645efb0dd1bfb9b08b2ca636
Dipanshu Mahato <dipanshumahato@gmail.com>
2022-12-01 13:55:50 +0530

    Initialised express server and exposed auth APIs

--- gardenify-backend/app.js
+++ gardenify-backend/app.js
@@ -60,3 +60,76 @@ io.on('connection', (socket) => {
   });
   // socket emmiters
   // --------MIDDLEWARES----------------
+  
+  // for parsing application/json
+  app.use(json());
+  // // for parsing application/x-www-form-urlencoded
+  app.use(urlencoded({ extended: false }));
+
+  // create statics middlewares
+  app.use(express.static('./views'));
+
+  // ----------REQUESTS-----------------
+  // signUp
+  app.post('/signup', (req, res) => {
+    const { name, mail, pass, latitude, longitude } = req.body;
+
+    const lat = Number(latitude);
+    const lng = Number(longitude);
+    if (
+      lat == null ||
+      lng == null ||
+      lat > 90 ||
+      lng > 180 ||
+      lat < -90 ||
+      lng < -180
+    ) {
+      res.json({ data: 'invalid-coordinates', success: false });
+    } else {
+      let uid;
+      signUp(mail, pass)
+        .then((data) => {
+          if (data.uid) {
+            uid = data.uid;
+          } else {
+            throw data;
+          }
+        })
+        .then(async () => {
+          createNewUser(uid, lat, lng, name)
+            .then((data) => {
+              // Joins room: UserID  which will listen to updates on plants
+              socket.join(uid);
+              res.json(data);
+            })
+            .catch((err) => {
+              console.log(err);
+              // TODO: Call firestore db by id and delete it.
+              res.json({ err, success: false });
+            });
+        })
+        .catch((err) => {
+          // console.log('ERROR IN SIGNUP: ', err);
+          res.json({ data: err, success: false });
+        });
+    }
+  });
+
+  // signIn
+  app.post('/signin', (req, res) => {
+    const { mail, pass } = req.body;
+    signIn(mail, pass)
+      .then((data) => {
+        if (data.uid) {
+          // Joins room: UserID which will listen to updates on plants
+          socket.join(data.uid);
+          res.json({ data: data.uid, success: true });
+        } else {
+          res.json({ data: data, success: false });
+        }
+      })
+      .catch((err) => {
+        console.log('FATAL ERROR: ', err);
+        res.json({ data: err, success: false });
+      });
+  });
