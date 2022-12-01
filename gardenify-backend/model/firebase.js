c285599093f026445587d3f55d7d9a1fb83f3abd
Dipanshu Mahato <dipanshumahato@gmail.com>
2022-12-01 14:20:20 +0530

    Implemented CRUD operations on collections: users/

--- gardenify-backend/model/firebase.js
+++ gardenify-backend/model/firebase.js
@@ -99,3 +99,162 @@ module.exports.signIn = async function (email, password) {
       return errorCode;
     });
 };
+
+// COLLECTIONS: USERS
+
+// get: plants
+module.exports.getPlant = async function (userID = null) {
+  try {
+    if (userID != null) {
+      throw { data: 'no id', success: false };
+    } else {
+      const docRef = doc(db, USERS_COL, userID);
+      const docSnap = await getDoc(docRef);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports.getUserDetails = async function (userID = null) {
+  try {
+    if (userID == null) {
+      throw { data: 'empty', success: false };
+    } else {
+      const docRef = doc(db, USERS_COL, userID);
+      const docSnap = await getDoc(docRef);
+      const data = docSnap.data();
+      if (data) return { data: docSnap.data(), success: true };
+      else {
+        throw 'no-user-found';
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    return { data: error, success: false };
+  }
+};
+
+// create
+module.exports.createNewUser = async function (
+  userID,
+  lat,
+  lng,
+  name = 'User'
+) {
+  try {
+    // compute date
+    const date = calculateDateRightNow();
+    // creating a new geopoint to set it to latitudes, and longitudes
+    // Compute the GeoHash for a lat/lng point
+    const latitude = Number(lat);
+    const longitude = Number(lng);
+    const hash = geohashForLocation([latitude, longitude]);
+
+    // Adding the hash and the lat/lng to the document to use the hash
+    // for queries and the lat/lng for distance comparisons.
+    let docData = {
+      id: userID,
+      name: name,
+      accountCreationDate: date,
+      geohash: hash,
+      latitude: latitude,
+      longitude: longitude,
+      points: 0,
+      plants: [],
+    };
+    const res = await setDoc(doc(db, USERS_COL, userID), docData);
+    // const newData = { id: userID, docData };
+    return { data: docData, success: true };
+  } catch (error) {
+    return { data: error, success: false };
+  }
+};
+
+// update plants field
+module.exports.updatePlantArrayOfUser = async function (userID, plantID) {
+  try {
+    const docRef = doc(db, USERS_COL, userID);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      var updatedList = [...docSnap.data().plants, plantID];
+      await updateDoc(docRef, { plants: updatedList });
+      return { data: plantID, success: true };
+    } else {
+      throw { data: 'no-such-user', success: false };
+    }
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports.increasePoints = async function (userID, point) {
+  try {
+    const docRef = doc(db, USERS_COL, userID);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      // convert points to number
+      const pointNumber = Number(point);
+      const updatedPoints = Number(pointNumber + docSnap.data().points);
+      await updateDoc(docRef, { points: updatedPoints });
+      return { data: { id: userID }, success: true };
+    } else {
+      throw { data: 'no-such-user', success: false };
+    }
+  } catch (error) {
+    return error;
+  }
+};
+// Get Nearby Users:
+module.exports.getNearbyUsers = async (lat, lng, dist = 1) => {
+  // Find cities within dist km of [lat, lng]
+  const center = [Number(lat), Number(lng)];
+  const radiusInM = dist * 1000;
+
+  // Each item in 'bounds' represents a startAt/endAt pair. We have to issue
+  // a separate query for each pair. There can be up to 9 pairs of bounds
+  // depending on overlap, but in most cases there are 4.
+  const bounds = geohashQueryBounds(center, radiusInM);
+
+  const promises = [];
+  for (const b of bounds) {
+    const q = query(
+      collection(db, USERS_COL),
+      orderBy('geohash'),
+      startAt(b[0]),
+      endAt(b[1])
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      promises.push(doc.data());
+    });
+  }
+
+  var finalResult = [];
+
+  // Collect all the query results together into a single list
+  await Promise.all(promises)
+    .then((snapshots) => {
+      const matchingDocs = [];
+
+      for (const snap of snapshots) {
+        // We have to filter out a few false positives due to GeoHash
+        // accuracy, but most will match
+        const lat = Number(snap.latitude);
+        const lng = Number(snap.longitude);
+        const distanceInKm = distanceBetween([lat, lng], center);
+        const distanceInM = distanceInKm * 1000;
+        if (distanceInM <= radiusInM) {
+          matchingDocs.push(snap);
+        }
+      }
+      return matchingDocs;
+    })
+    .then((matchingDocs) => {
+      // --operations
+      finalResult = matchingDocs;
+      // return matchingDocs;
+    });
+  console.log(finalResult);
+  return finalResult;
+};
